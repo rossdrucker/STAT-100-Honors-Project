0a492972ee64db44f3add07559536c641e16f7b7
rossdrucker <ross.a.drucker@gmail.com>
2018-01-15 18:01:39 -0600

    Update _deploy.sh

--- deployment/_deploy.sh
+++ deployment/_deploy.sh
@@ -1,10 +1,12 @@
-git config --global user.email "ross.a.drucker@gmail.com"
+# Set git config information
 git config --global user.name "Ross Drucker"
+git config --global user.email "ross.a.drucker@gmail.com"
 
+# Clone the repository's gh-pages branch
 git clone -b gh-pages \
   https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
   book-output
-  
+
 cd book-output
 
 cp -r ../_book/* ./
@@ -12,5 +14,5 @@ cp -r ../_book/* ./
 cp -r ../*.Rmd ./
 
 git add *
-git commit -a -m "Updating (${TRAVIS_BUILD_NUMBER})"
+git commit -a -m "Updating book (${TRAVIS_BUILD_NUMBER})"
 git push origin gh-pages
